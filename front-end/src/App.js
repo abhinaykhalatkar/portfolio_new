8a6d1abeca87a01a17c19008dd72e21e27f3e7f4
abhinayoffice <abhinaykhalatkar@gmail.com>
2023-07-27 16:27:08 +0200

    changes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,7 @@
 // import AboutPage from "./Pages/About/About";
 // import NotFound404 from "./Pages/NotFound404/NotFound404";
 import Navbar from './Components/NavBar/NavBar';
-import RenderRoutes from './Router/router';
+import RenderRoutes from './Context/router';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
@@ -12,7 +12,7 @@ function App() {
   return (
     <div>
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
           {RenderRoutes().map((route, index) => (
             <Route key={index} path={route.path} element={route.element} />
