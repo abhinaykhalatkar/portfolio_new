87e38de1f3540fab2145a36f58d7d88d876822ae
abhinaykhalatkar <abhinaykhalatkar@gmail.com>
2023-08-12 04:28:42 +0200

    animated background and bouncing heading

--- front-end/src/Pages/Home/Home.js
+++ front-end/src/Pages/Home/Home.js
@@ -1,25 +1,44 @@
 import "./Home.scss";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { ThemeContext } from "../../Context/ThemeContext/ThemeContext";
 import { PageAnimationContext } from "../../Context/PageAnimationContext/PageAnimationContext";
-import logo from "../../Assets/logoAK.svg";
-
+import AnimatedLogo from "../../Components/logoAnimated/AnimatedLogo";
+import BouncyText from "../../Components/Bouncy-text/BouncyText";
 export default function HomePage() {
   const { darkTheme } = useContext(ThemeContext);
-  const { pageVariants,pageTransition } = useContext(PageAnimationContext);
+  const { pageVariants, pageTransition } = useContext(PageAnimationContext);
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      setIsMobile(screenWidth <= 800);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <motion.div
       className={`p-Home ${darkTheme ? "" : "light"}`}
-      initial='initial'
-      animate='animate'
-      exit='exit'
+      initial="initial"
+      animate="animate"
+      exit="exit"
       variants={pageVariants}
       transition={pageTransition}
     >
-      <h1>Hey from HomePage</h1>
-      <img src={logo} alt="hi"/>
+      <BouncyText
+        name_class="home-heading"
+        font_size="4rem"
+        text="Full-Stack Software Developer"
+      />
+      {isMobile ? null : <AnimatedLogo name_class="logoBack" />}
     </motion.div>
   );
 }
